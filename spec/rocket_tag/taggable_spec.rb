663be1c956f8e99a93911a517f9de2c4750f9e80
Manuel Meurer <manuel.meurer@gmail.com>
2012-12-20 12:59:38 +0100

    Add specs for "exact" parameter

--- spec/rocket_tag/taggable_spec.rb
+++ spec/rocket_tag/taggable_spec.rb
@@ -283,6 +283,81 @@ describe TaggableModel do
         end
       end
 
+      describe 'exact parameter' do
+        let(:exact_tags) { ['foo', 'bar'] }
+
+        describe ':exact => true' do
+          let(:foo_model)         { TaggableModel.create(:skills => ['foo']) }
+          let(:foo_bar_model)     { TaggableModel.create(:skills => ['foo', 'bar']) }
+          let(:foo_bar_baz_model) { TaggableModel.create(:skills => ['foo', 'bar', 'baz']) }
+
+          it 'should not return records that are missing any of the speficied tags' do
+            TaggableModel.tagged_with(exact_tags, :exact => true).should_not include(foo_model)
+          end
+
+          it 'should not return records that have more than the specified tags' do
+            TaggableModel.tagged_with(exact_tags, :exact => true).should_not include(foo_bar_baz_model)
+          end
+
+          it 'should return records that have exactly the specificed tags' do
+            TaggableModel.tagged_with(exact_tags, :exact => true).should include(foo_bar_model)
+          end
+        end
+
+        describe ':exact => true, :on => context' do
+          let(:skills_foo_model)            { TaggableModel.create(:skills => ['foo']) }
+          let(:skills_foo_bar_model)        { TaggableModel.create(:skills => ['foo', 'bar']) }
+          let(:skills_foo_bar_baz_model)    { TaggableModel.create(:skills => ['foo', 'bar', 'baz']) }
+          let(:languages_foo_model)         { TaggableModel.create(:languages => ['foo']) }
+          let(:languages_foo_bar_model)     { TaggableModel.create(:languages => ['foo', 'bar']) }
+          let(:languages_foo_bar_baz_model) { TaggableModel.create(:languages => ['foo', 'bar', 'baz']) }
+
+          context 'with the correct context' do
+            let(:context) { :skills }
+
+            it 'should not return any records with incorrect context' do
+              [languages_foo_model, languages_foo_bar_model, languages_foo_bar_baz_model].each do |model|
+                TaggableModel.tagged_with(exact_tags, :exact => true, :on => context).should_not include(model)
+              end
+            end
+
+            it 'should not return records with correct context that are missing any of the speficied tags' do
+              TaggableModel.tagged_with(exact_tags, :exact => true, :on => context).should_not include(skills_foo_model)
+            end
+
+            it 'should not return records with correct context that have more than the specified tags' do
+              TaggableModel.tagged_with(exact_tags, :exact => true, :on => context).should_not include(skills_foo_bar_baz_model)
+            end
+
+            it 'should return records with correct context that have exactly the specificed tags' do
+              TaggableModel.tagged_with(exact_tags, :exact => true, :on => context).should include(skills_foo_bar_model)
+            end
+          end
+
+          context 'with the incorrect context' do
+            let(:context) { :languages }
+
+            it 'should not return any records with incorrect context' do
+              [skills_foo_model, skills_foo_bar_model, skills_foo_bar_baz_model].each do |model|
+                TaggableModel.tagged_with(exact_tags, :exact => true, :on => context).should_not include(model)
+              end
+            end
+
+            it 'should not return records with correct context that are missing any of the speficied tags' do
+              TaggableModel.tagged_with(exact_tags, :exact => true, :on => context).should_not include(languages_foo_model)
+            end
+
+            it 'should not return records with correct context that have more than the specified tags' do
+              TaggableModel.tagged_with(exact_tags, :exact => true, :on => context).should_not include(languages_foo_bar_baz_model)
+            end
+
+            it 'should return records with correct context that have exactly the specificed tags' do
+              TaggableModel.tagged_with(exact_tags, :exact => true, :on => context).should include(languages_foo_bar_model)
+            end
+          end
+        end
+      end
+
       describe "Experiments with AREL" do
         it "foo" do
           u_t = Arel::Table::new :users
