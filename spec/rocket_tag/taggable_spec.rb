9644dd5d51c6d34510ad340cabba99ea74de696d
Manuel Meurer <manuel.meurer@gmail.com>
2012-12-20 13:02:27 +0100

    Remove debug comments, whitespace, and newlines from specs, align indentation

--- spec/rocket_tag/taggable_spec.rb
+++ spec/rocket_tag/taggable_spec.rb
@@ -5,7 +5,7 @@ describe TaggableModel do
     clean_database!
     @model = TaggableModel.create
   end
-#=begin
+
   describe "parsing" do
     it "converts strings to arrays using ruby core lib CSV" do
       m = TaggableModel.new :skills => %q%hello, is it me, you are looking for, cat%
@@ -60,7 +60,7 @@ describe TaggableModel do
       @model.needs.should == ["a"]
     end
   end
-#=end
+
   describe "combining with active relation" do
     before :each do
       TaggableModel.create :name => "test 0", :needs => %w[x y z]
@@ -73,7 +73,7 @@ describe TaggableModel do
       TaggableModel.create :name => "app  2", :skills => %w[a b c]
       TaggableModel.create :name => "app  3", :skills => %w[a b c]
     end
- 
+
     it "should generate the correct results" do
       TaggableModel.tagged_with(%w[a b], :all=>true).count(:distinct => true).should == 6
       TaggableModel.tagged_with(%w[a b], :all=>true).where{name.like "app%"}.count(:distinct => true).should == 3
@@ -83,7 +83,6 @@ describe TaggableModel do
   end
 
   describe "querying tags" do
-
     before :each do
       @user0 = User.create :name => "brad"
       @user1 = User.create :name => "hannah"
@@ -129,54 +128,42 @@ describe TaggableModel do
     end
 
     describe "#tagged_with" do
-        it "should count the number of matched tags" do
+      it "should count the number of matched tags" do
+        r = TaggableModel.tagged_with(["a", "b", "german"]).all
+        r.find{|i|i.name == "00"}.tags_count.should == 3
+        r.find{|i|i.name == "01"}.tags_count.should == 3
+        r.find{|i|i.name == "10"}.tags_count.should == 1
+        r.find{|i|i.name == "11"}.tags_count.should == 1
+        r.find{|i|i.name == "21"}.tags_count.should == 1
 
-          #<TaggableModel id: 2, name: "00", type: nil, foo: "A"> - 3 - german, french, a, b, x
-          #<TaggableModel id: 3, name: "01", type: nil, foo: "B"> - 3 - german, italian, a, b, y
-          #<TaggableModel id: 4, name: "10", type: nil, foo: "A"> - 1 - a, c
-          #<TaggableModel id: 5, name: "11", type: nil, foo: "B"> - 1 - a, c
-          #<TaggableModel id: 7, name: "21", type: nil, foo: "B"> - 1 - german, jinglish, c, d
-           
-#           r = TaggableModel.tagged_with(["a", "b", "german"]).all.each do |m|
-#             puts "#{m.inspect} - #{m.tags_count} - #{m.tags.map(&:name).join ', '}"
-#           end
- 
-          r = TaggableModel.tagged_with(["a", "b", "german"]).all
-          r.find{|i|i.name == "00"}.tags_count.should == 3
-          r.find{|i|i.name == "01"}.tags_count.should == 3
-          r.find{|i|i.name == "10"}.tags_count.should == 1
-          r.find{|i|i.name == "11"}.tags_count.should == 1
-          r.find{|i|i.name == "21"}.tags_count.should == 1
+        # The 'group by' operation to generate the count tags should
+        # be opaque to downstream operations. Thus count should
+        # return the correct number of records
+        r = TaggableModel.tagged_with(["a", "b", "german"]).count.should == 5
 
-          # The 'group by' operation to generate the count tags should
-          # be opaque to downstream operations. Thus count should
-          # return the correct number of records
-          r = TaggableModel.tagged_with(["a", "b", "german"]).count.should == 5
+        # It should be possible to cascade active relation queries on
+        # the
+        r = TaggableModel.tagged_with(["a", "b", "german"]).
+          where{tags_count>2}.count.should == 2
 
-          # It should be possible to cascade active relation queries on
-          # the 
-          r = TaggableModel.tagged_with(["a", "b", "german"]).
-            where{tags_count>2}.count.should == 2
+        # The min option is a shortcut for a query on tags_count
+        r = TaggableModel.tagged_with(["a", "b", "german"], :min => 2).count.should == 2
 
-          # The min option is a shortcut for a query on tags_count
-          r = TaggableModel.tagged_with(["a", "b", "german"], :min => 2).count.should == 2
+        r = TaggableModel.tagged_with(["a", "b", "german"], :on => :skills).all
+        r.find{|i|i.name == "00"}.tags_count.should == 2
+        r.find{|i|i.name == "01"}.tags_count.should == 2
+        r.find{|i|i.name == "10"}.tags_count.should == 1
+        r.find{|i|i.name == "11"}.tags_count.should == 1
+        r.find{|i|i.name == "21"}.should be_nil
 
-          r = TaggableModel.tagged_with(["a", "b", "german"], :on => :skills).all
-          r.find{|i|i.name == "00"}.tags_count.should == 2
-          r.find{|i|i.name == "01"}.tags_count.should == 2
-          r.find{|i|i.name == "10"}.tags_count.should == 1
-          r.find{|i|i.name == "11"}.tags_count.should == 1
-          r.find{|i|i.name == "21"}.should be_nil
-
-          # It should be possible to narrow scopes with tagged_with
-          r = @user0.taggable_models.tagged_with(["a", "b", "german"], :on => :skills).all
-          r.find{|i|i.name == "00"}.tags_count.should == 2
-          r.find{|i|i.name == "01"}.should be_nil
-          r.find{|i|i.name == "10"}.tags_count.should == 1
-          r.find{|i|i.name == "11"}.should be_nil
-          r.find{|i|i.name == "21"}.should be_nil
-
-        end
+        # It should be possible to narrow scopes with tagged_with
+        r = @user0.taggable_models.tagged_with(["a", "b", "german"], :on => :skills).all
+        r.find{|i|i.name == "00"}.tags_count.should == 2
+        r.find{|i|i.name == "01"}.should be_nil
+        r.find{|i|i.name == "10"}.tags_count.should == 1
+        r.find{|i|i.name == "11"}.should be_nil
+        r.find{|i|i.name == "21"}.should be_nil
+      end
     end
 
     describe "#tagged_similar" do
@@ -186,13 +173,12 @@ describe TaggableModel do
 
         @t00.tagged_similar(:on => [:languages, :skills]).count.should == 4
 
-        # Effectively similar to specifying all the contexts in 
+        # Effectively similar to specifying all the contexts in
         # the on clause
         @t00.tagged_similar.count.should == 4
       end
 
-      it "should return similar items in the correct order with the correct tags_count" do 
-
+      it "should return similar items in the correct order with the correct tags_count" do
         # ----
         similar = @t00.tagged_similar(:on => :skills).all
         similar[0].id.should == @t01.id
@@ -225,7 +211,6 @@ describe TaggableModel do
         similar[2].tags_count.should == 1
         similar[3].tags_count.should == 1
       end
-
     end
 
     describe "#tagged_with" do
@@ -237,6 +222,7 @@ describe TaggableModel do
           q0.should include @t01
         end
       end
+
       describe ":all => false" do
         it "should return records where *any* tags match on any context" do
           q0 = TaggableModel.tagged_with(["a", "german"] ).all
@@ -366,40 +352,34 @@ describe TaggableModel do
           counts = l_t.
             group(l_t[:user_id]).
             project(
-              l_t[:user_id].as("user_id"), 
+              l_t[:user_id].as("user_id"),
               l_t[:user_id].count.as("count_all")
           ).as "foo"
-
-          #puts TaggableModel.joins("JOIN " + counts.to_sql ).to_sql
         end
 
         it "should" do
           u_t = Arel::Table::new :users
           l_t = Arel::Table::new :logs
-	
+
           counts = l_t.
             group(l_t[:user_id]).
             project(
-              l_t[:user_id].as("user_id"), 
+              l_t[:user_id].as("user_id"),
               l_t[:user_id].count.as("count_all")
             ).as "foo"
-          
+
           users = u_t.
             join(counts).
             on(u_t[:id].
             eq(counts[:user_id])).
             project("*").project(counts[:count_all])
-
-          # puts users.to_sql
-          
         end
       end
 
       describe "Using in subqueries" do
         it "should be possible to select the 'id' of the relation to use in a subquery" do
-
           q = TaggableModel.where do
-            id.in(TaggableModel.tagged_with(["a", "b"]).select{id}) & 
+            id.in(TaggableModel.tagged_with(["a", "b"]).select{id}) &
             id.in(TaggableModel.tagged_with(["c"]).select{id})
           end
           q.count.should == 2
@@ -408,9 +388,8 @@ describe TaggableModel do
             id.in(TaggableModel.tagged_with(["a", "b"]).select{id})
           end.count.should == 4
         end
-
       end
-      
+
       describe "#popular_tags" do
         it "should return correct list (and correctly ordered) of popular tags for class and context" do
           TaggableModel.popular_tags.all.length.should == RocketTag::Tag.all.count
@@ -428,14 +407,6 @@ describe TaggableModel do
 
       describe "tag cloud calculations" do
         it "should return tags on an association and the counts thereof" do
-#           @user0.taggable_models.popular_tags.each do |tag|
-#             puts "#{tag.name}\t#{tag.tags_count}"
-#           end
-#           puts "-------------"
-#           @user1.taggable_models.popular_tags.each do |tag|
-#             puts "#{tag.name}\t#{tag.tags_count}"
-#           end
-
           # Check that the tags_count on each tag is in
           # descending order.
           @user0.taggable_models.popular_tags.count.should == 8
@@ -451,9 +422,8 @@ describe TaggableModel do
           end
 
           # Sanity check the two queries are not identical
-          @user0.taggable_models.popular_tags.should_not == 
+          @user0.taggable_models.popular_tags.should_not ==
             @user1.taggable_models.popular_tags
-
         end
       end
     end
